f971d40eed1b1ff0752deaf0cf62dd38dd4186e1
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2026-02-01 13:22:08 +0530

    chore: initial project setup with CI and tooling

---
+++ node_modules/server/services/githubService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const searchIssues = async (query) => {
+  const res = await axios.get(
+    `https://api.github.com/search/issues?q=${query}`,
+    {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`
+      }
+    }
+  );
+  return res.data.items;
+};
